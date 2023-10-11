f62224ddb7b1745331d199ff572a1961df6430ac
zhishun lin <zhislin@icloud.com>
2023-10-11 18:07:01 +0800

    update entry function docs

--- jsonextend.go
+++ jsonextend.go
@@ -1,30 +1,22 @@
 package jsonextend
 
 import (
-	"errors"
 	"io"
 
 	"github.com/jaksonlin/go-jsonextend/interpreter"
-	"github.com/jaksonlin/go-jsonextend/tokenizer"
 )
 
+// parse a jsonextend document with the variables into json bytes.
 func Parse(reader io.Reader, variables map[string]interface{}) ([]byte, error) {
-	sm := tokenizer.NewTokenizerStateMachineFromIOReader(reader)
-	err := sm.ProcessData()
-	if err != nil {
-		return nil, err
-	}
-	if sm.GetASTBuilder().HasOpenElements() {
-		return nil, errors.New("invalid json")
-	}
-	ast := sm.GetAST()
-	return interpreter.PrettyInterpret(ast, variables, Marshal)
+	return interpreter.ParseJsonExtendDocument(reader, variables)
 }
 
+// unmarshal a jsonextend document with the variables into a struct. should alied with json.Unmarshal
 func Unmarshal(reader io.Reader, variables map[string]interface{}, out interface{}) error {
 	return interpreter.Unmarshal(reader, variables, out)
 }
 
+// marshal a struct into json bytes. should alied with json.Marshal
 func Marshal(v interface{}) ([]byte, error) {
 	return interpreter.Marshal(v)
 }
